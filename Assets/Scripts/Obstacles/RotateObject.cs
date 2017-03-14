44e4610eaaf8178a5686ce2ca4794a31ce7815a5
Zac Olsen <zolsen@masonlive.gmu.edu>
2017-03-13 23:01:48 -0400

    added stool and box fan

--- Assets/Scripts/Obstacles/RotateObject.cs
+++ Assets/Scripts/Obstacles/RotateObject.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 public class RotateObject : MonoBehaviour {
 
 	[SerializeField] private float rotationAngleSec = 360f;
-	
+	[SerializeField] RotationDirection direction = RotationDirection.Y;
+
+	private enum RotationDirection {X, Y, Z};
+
 	void FixedUpdate () {
-		transform.RotateAround (transform.position, Vector3.up, rotationAngleSec * Time.fixedDeltaTime);
+
+		Vector3 vec = Vector3.forward;
+
+		if (direction == RotationDirection.X) {
+			vec = Vector3.right;
+		} else if (direction == RotationDirection.Y) {
+			vec = Vector3.up;
+		}
+
+		transform.RotateAround (transform.position, transform.parent.TransformDirection (vec),
+			rotationAngleSec * Time.fixedDeltaTime);
 	}
 }
