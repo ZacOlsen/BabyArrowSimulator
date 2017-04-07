d1a7ce62ecf5a57e64ad0510983ed6da882b9505
Zac Olsen <zolsen@masonlive.gmu.edu>
2017-04-07 16:14:51 -0400

    ui updates

--- Assets/Scripts/LevelStartText.cs
+++ Assets/Scripts/LevelStartText.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelStartText : MonoBehaviour {
 
+	[TextArea(3,10)][SerializeField] private string[] texts = null;
+
+	private Text text;
 	private static bool shown;
+	private int index;
 
 	void Start () {
 
@@ -13,11 +18,20 @@ public class LevelStartText : MonoBehaviour {
 		} else {
 			gameObject.SetActive (false);
 		}
+
+		text = GetComponentInChildren<Text> ();
+		text.text = texts [index];
 	}
 	
 	void Update () {
 
 		if (Input.GetMouseButtonDown (0)) {
+
+			index++;
+			if (index < texts.Length) {
+				text.text = texts [index];
+				return;
+			}
 
 			shown = true;
 			Time.timeScale = 1f;
